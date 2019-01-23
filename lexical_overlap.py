b1241d1fcdb0527870c9a2cdc8b7045ecb4c5044
LorenBristow <lorendup@gmail.com>
2019-01-23 21:51:05 +0000

    part automated lexical comparison

--- lexical_overlap.py
+++ lexical_overlap.py
@@ -9,6 +9,27 @@ dict_a = {}
 dict_b = {} 
 overlapScore = 0
 
+def scoreLexicalOverlap(a,b):
+    extractTexta(a)
+    extractTextb(b)
+    count_of_overlapping_words = 0
+    count_of_non_overlapping_words_ie_in_a_but_not_b = 0 
+    for word in dict_a:
+        if word in dict_b:
+            count_of_overlapping_words += 1 # is overlap value magnified when x occurs many times in a, and only once in b?
+       #is the number different when reversing which text is a or b?
+        else:
+            count_of_non_overlapping_words_ie_in_a_but_not_b += 1
+#    print(count_of_overlapping_words)
+#    print(count_of_non_overlapping_words_ie_in_a_but_not_b)
+    recon = len(dict_a) - (count_of_overlapping_words + count_of_non_overlapping_words_ie_in_a_but_not_b)
+    #print("recon ", recon) #check - should be zero. #maybe add a test to check this and return verdict?
+    overlapScore = round(count_of_overlapping_words / ((len(dict_a) + len(dict_b)) - count_of_overlapping_words),3)
+    print("compare: (george0{}.txt <> george0{}.txt) = ".format(a,b), overlapScore)
+    return overlapScore
+#scoreLexicalOverlap(1,2) # takes in the file number of each of the two dictionaries to be compared. 
+#AUTOMATE WHAT YOU FEED IT. ie for loop through 1-4 files and logic for which to compare. send to DB?
+
 def extractTexta(a):  
     fname = open("george0{}.txt".format(a))
     for line in fname:
@@ -35,64 +56,22 @@ def extractTextb(b):
 #    print(len(dict_b))
     return dict_b
 
-def scoreLexicalOverlap(a,b):
-    extractTexta(a)
-    extractTextb(b)
-    count_of_overlapping_words = 0
-    count_of_non_overlapping_words_ie_in_a_but_not_b = 0 
-    for word in dict_a:
-        if word in dict_b:
-            count_of_overlapping_words += 1 # is overlap value magnified when x occurs many times in a, and only once in b?
-       #is the number different when reversing which text is a or b?
-        else:
-            count_of_non_overlapping_words_ie_in_a_but_not_b += 1
-#    print(count_of_overlapping_words)
-#    print(count_of_non_overlapping_words_ie_in_a_but_not_b)
-    recon = len(dict_a) - (count_of_overlapping_words + count_of_non_overlapping_words_ie_in_a_but_not_b)
-    #print("recon ", recon) #check - should be zero. #maybe add a test to check this and return verdict?
-    overlapScore = count_of_overlapping_words / ((len(dict_a) + len(dict_b)) - count_of_overlapping_words)
-    print("compare: (george0{}.txt <> george0{}.txt) = ".format(a,b), overlapScore)
-    return overlapScore
-#scoreLexicalOverlap(1,2) # takes in the file number of each of the two dictionaries to be compared. 
-#AUTOMATE WHAT YOU FEED IT. ie for loop through 1-4 files and logic for which to compare. send to DB?
+def startTheEngine():
+    listOfTextsByNumberPLUSONE = [1,2,3,4,5] # list must include 1 more than actual number of files. Needs to think more on this. 
+    for number in listOfTextsByNumberPLUSONE:
+        for unique_pairs in range(number + 1, len(listOfTextsByNumberPLUSONE)):
+            overlapScore = scoreLexicalOverlap(number,unique_pairs) #running the function and assigning return directly to variable. 
+startTheEngine()
 
 
-for number in range(0,3):
-    overlapScore = scoreLexicalOverlap(number + 1,number + 2) #running the function and assigning return directly to variable. 
-    
-
-###############################################
-#scoreLexicalOverlap(a,b) # takes in the file number of each of the two dictionaries to be compared. 
-
-
-#def create_dictionaries(1,2):
-#    for number in range(1,5):
-##        name_dict = "d{}".format(number)
-#        d = {}
-#        fname = open("george0{}.txt".format(number))
-#        for line in fname:
-#            words = line.split()
-#            for word in words:
-#                if word in d:
-#                   d[word] += 1
-#                else:
-#                    d[word] = 1
-#    print("TEXT {}".format(number), d)
-#    return {}
-
-#counted_words = {}   
-#
-#def countWords(stopwords):     
-#    fname = open('mobydick.txt', 'r')    
-#    
-#    for line in fname:
-#        words = line.split()
-#        for word in words:
-#            if word not in stopwords:
-#                if word in counted_words:
-#                    counted_words[word] = counted_words[word] + 1
-#                else:
-#                    counted_words[word] = 1
-#    print("YYYYY",counted_words)
-#
-#countWords(readStopWords())
+#####WORK IN PROGRESS - USER INPUT TO DRIVE NUMBER OF FILES COMPARED###    
+#def startTheEngine():
+#    listOfTextsByNumberPLUSONE = []
+#    number_of_texts = int(input("How many files are to be compared?\n"))
+#    for num in range(1,number_of_texts + 1):
+#        listOfTextsByNumberPLUSONE.append(num) # list must include 1 more than actual number of files. Needs to think more on this. 
+#        #print(listOfTextsByNumberPLUSONE)
+#        for number in listOfTextsByNumberPLUSONE:
+#            for unique_pairs in range(number + 1, len(listOfTextsByNumberPLUSONE)):
+#                overlapScore = scoreLexicalOverlap(number,unique_pairs) #running the function and assigning return directly to variable. 
+#startTheEngine()
